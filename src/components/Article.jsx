1c282272e8d02d75edb4ca063b02f30448b10663
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-07-02 15:30:12 +0530

    Worked on Article Layout

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Article.css";
 import dashboard from '../Assets/dashboard.jpg';
+import arrow from '../Assets/right-arrow.png';
+
 function Article() {
   return (
     <div className="article-wrapper">
@@ -9,18 +11,22 @@ function Article() {
       </div>
       <div className="extra-content">
         <div className="left-content">
-          <p className="heading">
+          <p className="left-heading">
             We help companies make the most of their digital experiences.
           </p>
-          <p className="data">
+          <p className="left-data">
             You’re not looking for a vendor. You’re looking for a partner. One
             who wants to understand your challenges—all of them—in order to make
             your project successful. (And we know that not all of those are
             solved with code). Because you don’t just need a solution. You need
             the best solution.
           </p>
+          <div className="btn-container">
+                <img src={arrow} alt="right-arrow" />
+                Learn more About me...
+          </div>
         </div>
-        <div className="right">
+        <div className="right-content">
             <img className="right-img" src={dashboard} alt="" />
         </div>
       </div>
