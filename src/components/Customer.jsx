9d7abf2caddb30bb68308e13952fba5775b25734
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-07-03 15:51:34 +0530

    Added Cust review cards

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -18,9 +18,94 @@ function Customer() {
         </button>
       </div>
       <div className="cust-card">
-        <div className="cards card1"></div>
-        <div className="cards card2"></div>
-        <div className="cards card3"></div>
+        <div className="cards card1">
+          <div className="title">
+            <svg
+              width="19"
+              height="18"
+              viewBox="0 0 36 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M36 7.26486C29.2776 7.26486 26.9779 9.85946 26.9779 13.7514V16.9514H35.7346V32H19.9017V14.1838C19.9017 5.27567 25.2088 0 36 0V7.26486ZM0 32V14.1838C0 5.27567 5.30713 0 16.0983 0V7.26486C9.37592 7.26486 7.07617 9.85946 7.07617 13.7514V16.9514H15.8329V32H0Z"
+                fill="#FF0EB4"
+              ></path>
+            </svg>
+            <img
+              src="https://cdn.prod.website-files.com/65b671b80c07b825f14b65c6/6614d2b8bf7ea0b8033d40bb_conde-nast-logo-black%201.svg"
+              alt=""
+            />
+          </div>
+          <div className="cards-data">
+            <p>
+              Avo has been a tremendous help for us. For a company as big as
+              ours, we need Avo to help us standardize all of our data across a
+              lot of platforms, brands, and markets, no matter where it is
+              coming from.
+            </p>
+            <p>Read more reviews on G2</p>
+          </div>
+        </div>
+        <div className="cards card2">
+          <div className="title">
+            <svg
+              width="19"
+              height="18"
+              viewBox="0 0 36 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M36 7.26486C29.2776 7.26486 26.9779 9.85946 26.9779 13.7514V16.9514H35.7346V32H19.9017V14.1838C19.9017 5.27567 25.2088 0 36 0V7.26486ZM0 32V14.1838C0 5.27567 5.30713 0 16.0983 0V7.26486C9.37592 7.26486 7.07617 9.85946 7.07617 13.7514V16.9514H15.8329V32H0Z"
+                fill="#FF0EB4"
+              ></path>
+            </svg>
+            <img
+              src="https://cdn.prod.website-files.com/65b671b80c07b825f14b65c6/6614d2b8bf7ea0b8033d40bb_conde-nast-logo-black%201.svg"
+              alt=""
+            />
+          </div>
+          <div className="cards-data">
+            <p>
+              Avo has been a tremendous help for us. For a company as big as
+              ours, we need Avo to help us standardize all of our data across a
+              lot of platforms, brands, and markets, no matter where it is
+              coming from.
+            </p>
+            <p>Read more reviews on G2</p>
+          </div>
+        </div>
+        <div className="cards card3">
+          <div className="title">
+            <svg
+              width="19"
+              height="18"
+              viewBox="0 0 36 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M36 7.26486C29.2776 7.26486 26.9779 9.85946 26.9779 13.7514V16.9514H35.7346V32H19.9017V14.1838C19.9017 5.27567 25.2088 0 36 0V7.26486ZM0 32V14.1838C0 5.27567 5.30713 0 16.0983 0V7.26486C9.37592 7.26486 7.07617 9.85946 7.07617 13.7514V16.9514H15.8329V32H0Z"
+                fill="#FF0EB4"
+              ></path>
+            </svg>
+            <img
+              src="https://cdn.prod.website-files.com/65b671b80c07b825f14b65c6/6614d2b8bf7ea0b8033d40bb_conde-nast-logo-black%201.svg"
+              alt=""
+            />
+          </div>
+          <div className="cards-data">
+            <p>
+              Avo has been a tremendous help for us. For a company as big as
+              ours, we need Avo to help us standardize all of our data across a
+              lot of platforms, brands, and markets, no matter where it is
+              coming from.
+            </p>
+            <p>Read more reviews on G2</p>
+          </div>
+        </div>
+      
       </div>
     </div>
   );
