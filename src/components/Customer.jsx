c746556de005245a4991dec767d5874d938b45df
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-07-03 15:25:47 +0530

    card,btn of cust section

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Customer.css";
+import arrow from "../Assets/right-arrow.png";
 
 function Customer() {
   return (
     <div className="customer-reviews">
-    <div className="cust-title">
-      Accelerate your business. The world's most effective teams build with
-      Dev <strong>S I D</strong>.
+      <div className="cust-title">
+        <p className="title-text">
+          Accelerate your business. The world's most effective teams build with
+          Dev - S I D.
+        </p>
+      </div>
+      <div className="cust-btn">
+        <button>
+          <p>Reviews</p>
+          <img src={arrow} alt="right arrow" />
+        </button>
+      </div>
+      <div className="cust-card">
+        <div className="cards card1"></div>
+        <div className="cards card2"></div>
+        <div className="cards card3"></div>
+      </div>
     </div>
-    <div className="cust-btn">Reviews</div>
-    <div className="cust-card"></div>
-  </div>
-  )
+  );
 }
 
-export default Customer
+export default Customer;
